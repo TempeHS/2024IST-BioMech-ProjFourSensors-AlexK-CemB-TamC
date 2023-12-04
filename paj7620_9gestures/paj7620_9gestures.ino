a061f401660f8a75fbd140f641b961d6d7b6d491
timthetamman <127055360+timthetamman@users.noreply.github.com>
2023-12-05 10:33:27 +1100

    update code

--- paj7620_9gestures/paj7620_9gestures.ino
+++ paj7620_9gestures/paj7620_9gestures.ino
@@ -1,34 +1,6 @@
-/*
-    Copyright (c) 2023 seeed technology inc.
-    Website    : www.seeed.cc
-    Author     : Wuruibin
-    Modified Time: June 2023
-    Description: This demo can recognize 9 gestures and output the result, including move up, move down, move left, move right,
-  				move forward, move backward, circle-clockwise, circle-counter clockwise, and wave.
-
-    The MIT License (MIT)
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-*/
 
 #include "Gesture.h"
-#include <Keyboard.h>
+
 /*
     Notice: When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s).
         You also can adjust the reaction time according to the actual circumstance.
@@ -43,7 +15,6 @@ void setup() {
   cemSetup ();
   alexSetup ();
   tamSetup ();
-  keyboardSetup ();
 
     Serial.begin(9600);
     while(!Serial) {
